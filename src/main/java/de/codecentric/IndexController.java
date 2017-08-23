96d0ef58bd1332789f700cc2f02f318462960f5e
Paul Piper <paul.piper@codecentric.de>
2017-08-23 17:06:03 +0200

    Implemented the Hello World View

--- src/main/java/de/codecentric/IndexController.java
+++ src/main/java/de/codecentric/IndexController.java
@@ -1,19 +1,15 @@
 package de.codecentric;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@RestController
 
 public class IndexController {
     @RequestMapping("/")
-    public String start(@RequestParam(value = "name", required = false, defaultValue = "you cute User") String name, Model model) {
-        model.addAttribute("name", name);
-        return "start";
+    public String start() {
+        return "hello world";
     }
-
 }
 
 
