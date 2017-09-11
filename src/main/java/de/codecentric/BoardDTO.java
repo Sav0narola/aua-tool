2e6ba1d7bc4ad7f26d6d910913e8d33127e0d938
Paul Piper <paul.piper@codecentric.de>
2017-09-11 11:10:18 +0200

    Implementing simple BoardObject

---
+++ src/main/java/de/codecentric/BoardDTO.java
@@ -0,0 +1,18 @@
+package de.codecentric;
+
+public class BoardDTO {
+
+    public BoardDTO (String title){
+        this.title = title;
+    }
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- src/main/java/de/codecentric/IndexController.java
+++ src/main/java/de/codecentric/IndexController.java
@@ -3,13 +3,22 @@ package de.codecentric;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 @RestController
 
 public class IndexController {
+
     @RequestMapping("/")
     public String start() {
         return "hello world";
     }
+
+    @RequestMapping("/boards")
+    public List<BoardDTO> getBoards() {
+        return asList(new BoardDTO("hello"), new BoardDTO("world"));
+    }
 }
-
-
